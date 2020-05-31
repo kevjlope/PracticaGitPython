ea968737f5a4fa7ac1b949192aee44dc3af97087
kevjlope <kevinjeff04@gmail.com>
2020-05-30 20:16:45 -0500

    Anotando configuracion y comandos basicos de git, en el archivo

--- practica.py
+++ practica.py
@@ -1,1 +1,7 @@
 print("Hola mundo en python de nuevo ajaja xD")
+#Explicar directorio, staging, repositorio
+#Antes de crear el primer commit se debe configurar git por lo menos (git config --list)
+#con nombre de user and email comando: git config --global user.name// user.email
+#Pasos git add nombreArchivo, git commit -m "Comentario"
+#Para regresar un archivo de staging a directorio --> git rm -- cached "nombreArchivo"
+# git log nombreArchivo --> Para ver el historial de los commits de cada commit
