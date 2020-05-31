a0b0fcb4f0420505ccd2eb0355dc711a51f233f2
kevjlope <kevinjeff04@gmail.com>
2020-05-30 20:42:18 -0500

    Agg mas informacion de practica

--- practica.py
+++ practica.py
@@ -3,9 +3,14 @@ print("Hola mundo en python de nuevo ajaja xD")
 #Antes de crear el primer commit se debe configurar git por lo menos (git config --list)
 #con nombre de user and email comando: git config --global user.name// user.email
 #Pasos git add nombreArchivo, git commit -m "Comentario"
-#Para regresar un archivo de staging a directorio --> git rm -- cached "nombreArchivo"
+#Para regresar un archivo de staging a directorio --> git rm --cached "nombreArchivo"
 # git log nombreArchivo --> Para ver el historial de los commits de cada commit
 
 #*Analizar cambios de un archivo*#
 #git show nombreArchivo
 
+#*Regresar a commits anteriores*
+#git reset sadsada(host) --hard/soft --> el mas utilizado es hard
+#Pero ojo se elimina lo que se habia hecho con hard
+# git checkout asdasdas(hosts) nombreArchivo --> Regresa en el tiempo en el host indicado
+# git checkout master 
