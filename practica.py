d43951b85b6284249e64e1bbf042b65c91317ab3
kevjlope <kevinjeff04@gmail.com>
2020-05-30 21:11:03 -0500

    Bueno

--- practica.py
+++ practica.py
@@ -8,9 +8,12 @@ print("Hola mundo en python de nuevo ajaja xD")
 
 #*Analizar cambios de un archivo*#
 #git show nombreArchivo
+# git diff host1 host2
 
 #*Regresar a commits anteriores*
 #git reset sadsada(host) --hard/soft --> el mas utilizado es hard
 #Pero ojo se elimina lo que se habia hecho con hard
 # git checkout asdasdas(hosts) nombreArchivo --> Regresa en el tiempo en el host indicado
-# git checkout master 
+# git checkout master nombreArchivo --> sinceramente no me agrada esta forma
+#es mas practico crear otra rama y realizar ahi los experimentos y despues enviarlo a produccion
+
